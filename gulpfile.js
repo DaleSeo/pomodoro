e24ec0bc479b6f07491403b544eb65ae535d4191
Dale Seo <dale.seo@gmail.com>
2016-10-05 13:47:31 +0900

    gulp bableify & init react

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
+var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
+gulp.task('default', ['browserify']);
+
 gulp.task('browserify', function() {
   console.log('Gulp! Browserify!!');
-  return browserify('./src/main.js').bundle()
+  return browserify('./src/main.js')
+    .transform(babelify, {presets: "react"})
+    .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./public/javascripts'));
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-/* external module */
-var unique = require('bootstrap');
-console.log(unique(data));
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+  render: function () {
+    // return <div><h1>Hello React</h1></div>;
+    return React.createElement('div', null, 'Hello React');
+  }
+});
+
+ReactDOM.render(<Main />, document.getElementById('main'));
