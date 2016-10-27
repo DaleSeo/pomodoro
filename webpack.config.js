617c0b68e567ef99dd7e2b06af13e5468f3711ed
Dale Seo <dale.seo@gmail.com>
2016-10-27 21:28:55 +0900

    remove gulp

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: "./src/index.js",
   output: {
+    path: __dirname,
     filename: "./public/javascripts/bundle.js"
   },
   module: {
