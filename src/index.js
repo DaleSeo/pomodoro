d1b4b931863db35821380ae6bfca0b81d6727d73
Dale Seo <dale.seo@gmail.com>
2016-10-28 12:27:02 +0900

    fix font-awesome-webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 require('expose?$!expose?jQuery!jquery');
 require("bootstrap-webpack");
-require('font-awesome/css/font-awesome.css');
+require("font-awesome-webpack");
 require('./main');
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: __dirname + "/public",
-    filename: "bundle.js",
-    publicPath: "/public"
+    filename: "bundle.js"
   },
   module: {
     loaders: [
@@ -21,26 +20,8 @@ module.exports = {
         test: /\.css$/,
         loader: 'style!css?sourceMap'
       },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream"
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml"
-      }
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   }
 }
